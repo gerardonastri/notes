b4b8613a898633ee9709f73854008a765aecb351
gerardonastri <gerardonastri06@gmail.com>
2022-06-12 09:22:01 +0200

    better

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -10,7 +10,10 @@ const  BASE_URL = "https://jer-notes-api.herokuapp.com/api/";
 
 const axiosReq = axios.create({
     baseURL:  BASE_URL,
-    headers: {"Access-Control-Allow-Origin": "*"}
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+    }
     //headers: {
      //   'X-CSRFTOKEN': csrftoken
     //},
